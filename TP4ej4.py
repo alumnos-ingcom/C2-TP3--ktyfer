f1e4404e6ef92ec03428126bb22a3879e193d019
ktyfer <kty.fer.06@outlook.com>
2021-06-15 20:16:00 -0300

    correccion comparacion de numeros

--- TP4ej4.py
+++ TP4ej4.py
@@ -6,9 +6,11 @@
 
 #comparacion de numeros
 
-'''Esta funcion me dice si el numero 1 es menor que numero2'''
+
 
 def compara(num1, num2):
+    '''Esta funcion me dice si el numero 1 es menor que numero 2'''
+    
     if num1 < num2:
         return ('-1')
     
@@ -22,7 +24,9 @@ def compara(num1, num2):
 def prueba():
     num1 = int(input('Ingrese un numero: '))
     num2  = int(input('ingrese un numero: '))
+    
     num_ingresado = compara(num1, num2)
+    
     print (f'La comparacion de numeros es: {compara(num1, num2)}')
     
 
